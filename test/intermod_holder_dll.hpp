0a498596626a4008445551f7bd7cbdbc77f555d4
Nikita Kniazev <nok.raven@gmail.com>
2018-10-25 16:04:17 +0300

    TST: Fixed intermod_holder_dll symbols visibility

--- test/intermod_holder_dll.hpp
+++ test/intermod_holder_dll.hpp
@@ -23,14 +23,10 @@
 #include <boost/flyweight/simple_locking.hpp>
 #include <string>
 
-#ifdef BOOST_HAS_DECLSPEC
 #ifdef BOOST_FLYWEIGHT_TEST_INTERMOD_HOLDER_DLL_SOURCE
-#define BOOST_FLYWEIGHT_DLL_DECL __declspec(dllexport)
+#  define BOOST_FLYWEIGHT_DLL_DECL BOOST_SYMBOL_EXPORT
 #else
-#define BOOST_FLYWEIGHT_DLL_DECL __declspec(dllimport)
-#endif
-#else
-#define BOOST_FLYWEIGHT_DLL_DECL
+#  define BOOST_FLYWEIGHT_DLL_DECL BOOST_SYMBOL_IMPORT
 #endif
 
 typedef boost::flyweights::flyweight<
