c15f7f021a309dd97b393e70b27b882473a400d0
Peter Dimov <pdimov@gmail.com>
2023-02-20 17:36:40 +0200

    Remove serialization includes

--- include/boost/flyweight/serialize.hpp
+++ include/boost/flyweight/serialize.hpp
@@ -17,17 +17,17 @@
 #include <boost/flyweight/flyweight_fwd.hpp>
 #include <boost/flyweight/detail/archive_constructed.hpp>
 #include <boost/flyweight/detail/serialization_helper.hpp>
-#include <boost/serialization/nvp.hpp>
-#include <boost/serialization/split_free.hpp>
-#include <boost/throw_exception.hpp> 
+#include <boost/core/serialization.hpp>
+#include <boost/throw_exception.hpp>
 #include <memory>
+#include <stdexcept>
 
 /* Serialization routines for flyweight<T>. 
  */
 
 namespace boost{
-  
-namespace serialization{
+
+namespace flyweights{
 
 template<
   class Archive,
@@ -37,7 +37,7 @@ inline void serialize(
   Archive& ar,::boost::flyweights::flyweight<T,Arg1,Arg2,Arg3>& f,
   const unsigned int version)
 {
-  split_free(ar,f,version);              
+  core::split_free(ar,f,version);
 }                                               
 
 template<
@@ -81,7 +81,7 @@ void load(
   ar>>make_nvp("item",n);
   if(n>hlp.size()){
     throw_exception(
-      archive::archive_exception(archive::archive_exception::other_exception));
+      std::runtime_error( "Invalid or corrupted archive" ) );
   }
   else if(n==hlp.size()){
     ::boost::flyweights::detail::archive_constructed<key_type> k(
@@ -91,7 +91,7 @@ void load(
   f=hlp[n];
 }
 
-} /* namespace serialization */
+} /* namespace flyweights */
 
 } /* namespace boost */
 
