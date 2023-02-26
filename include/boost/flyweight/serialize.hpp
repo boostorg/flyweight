1a1411e9d430e586a2d0134a566e1948edf9192a
joaquintides <joaquin@tid.es>
2023-02-26 19:33:29 +0100

    updated (C) year, editorial

--- include/boost/flyweight/serialize.hpp
+++ include/boost/flyweight/serialize.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2006-2015 Joaquin M Lopez Munoz.
+/* Copyright 2006-2023 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
@@ -80,8 +80,7 @@ void load(
   size_type n=0;
   ar>>make_nvp("item",n);
   if(n>hlp.size()){
-    throw_exception(
-      std::runtime_error( "Invalid or corrupted archive" ) );
+    throw_exception(std::runtime_error("Invalid or corrupted archive"));
   }
   else if(n==hlp.size()){
     ::boost::flyweights::detail::archive_constructed<key_type> k(
